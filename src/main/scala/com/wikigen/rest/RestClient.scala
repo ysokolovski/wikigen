5d7e39df5c39fbb3a381353ab1c257103b6b2206
Yuri Sokolovski <zarodiny@gmail.com>
2015-01-17 14:53:23 -0500

    Working RestClientTest for GET

--- src/main/scala/com/wikigen/rest/RestClient.scala
+++ src/main/scala/com/wikigen/rest/RestClient.scala
@@ -1,18 +1,16 @@
 package com.wikigen.rest
 
-import javax.annotation.Resource
-
-import com.wikigen.config.WikiGenConfig
-
-import scala.util.parsing.json.{JSON, JSONObject}
+import scala.util.parsing.json.JSON
 
 /**
  * Created by Yuri on 11/01/2015.
  */
-class RestClient (connection:HttpConnection) {
-  def get(resourceUri: String) : Option[Any] = {
-    val responseBody = connection.get(resourceUri)
-    JSON.parseFull(responseBody)
+class RestClient (baseUrl:String, connection:HttpConnection) {
+
+  def get(resourceUri: String) : Map[String,Any] = {
+    val responseBody = connection.get(baseUrl+resourceUri)
+    val parsed : Some[Map[String,Any]] = JSON.parseFull(responseBody).asInstanceOf[Some[Map[String,Any]]]
+    return parsed.get
   }
 
 }
--- src/test/scala/com/wikigen/rest/test/RestClientTest.scala
+++ src/test/scala/com/wikigen/rest/test/RestClientTest.scala
@@ -1,7 +1,7 @@
 package com.wikigen.rest.test
 
-import com.wikigen.rest.{RestClient, HttpConnection}
-import org.junit.Test
+import com.wikigen.rest.{HttpConnection, RestClient}
+import org.junit.Assert._
 import org.scalamock.scalatest.MockFactory
 import org.scalatest.FlatSpec
 
@@ -10,13 +10,14 @@ import org.scalatest.FlatSpec
  */
 class RestClientTest extends FlatSpec with MockFactory  {
 
-    val mockConnection=stub[HttpConnection];
+    "Rest Client" should "return JSON object when requested by ID" in {
 
+        val mockConnection = stub[HttpConnection];
 
-
-    val restClient = new RestClient(mockConnection)
-    val resource = restClient.get("/resource/1")
-    assertEquals("1", response.id)
-    assertEquals("name", response.name)
-  }
+        val restClient = new RestClient("http://fullurl",mockConnection)
+        (mockConnection.get _) when ("http://fullurl/resource/1") returns ("{\"id\":\"1\", \"name\":\"name\"}");
+        val response: Map[String,Any]= restClient.get("/resource/1")
+        assertEquals("1", response("id"))
+        assertEquals("name", response("name"))
+    }
 }
